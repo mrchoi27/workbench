31b946f057c03b7c229cd3987d42aba3a5c65192
Mr.Choi <mrchoi27@gmail.com>
2025-02-27 16:15:35 +0900

    Modify run-svr.sh

--- hacky-scripts/run-svr.sh
+++ hacky-scripts/run-svr.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-~/qemu/build/qemu-system-x86_64 -cpu host -enable-kvm -smp $(nproc) -m 8G \
-    -drive file=system.qcow,format=qcow2,if=virtio,id=boot \
+: ${QEMU_PATH:=~/hacky-qemu/build}
+
+QEMU=$QEMU_PATH/qemu-system-x86_64
+
+if [[ $(uname -a) == *""microsoft* ]] ; then
+    echo "WSL!!!"
+fi
+
+# sudo $QEMU -cpu host -enable-kvm -smp $(nproc) -m 8G \
+
+sudo $QEMU -smp $(nproc) -m 8G \
+    -drive file=system.qcow,format=qcow2,if=none,id=boot \
     -device ahci,id=ahci \
     -device ide-hd,drive=boot,bus=ahci.0 \
     -drive file=sata.raw,format=raw,if=none,id=sata \
