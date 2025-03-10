cf9bff8422a20ca7fd6a8331edd30f706a620df5
Mr.Choi <mrchoi27@gmail.com>
2025-03-10 16:48:25 +0900

    Modify misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -3,9 +3,22 @@
 qemu-img create -format raw sata.raw 10G
 qemu-img create -format raw nvme.raw 10G
 
-# build qemu
-./configure --target-list=x86_64-softmmu --enable-slirp
+
+# build qemu on linux - https://wiki.qemu.org/Hosts/Linux
+## required packages
+sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+sudo apt install at-spi2-core
+
+# configure
+./configure --target-list=x86_64-softmmu --enable-slirp --enable-debug
+
+# compile
 make -j $(nproc)
 
-# misc
-sudo apt install at-spi2-core
+
+# build qemu on macOS - https://wiki.qemu.org/Hosts/Mac
+## required packages
+libffi, gettext, glib, pkg-config, pixman, ninja, meson
+
+# compile
+make -j $(sysctl -h hw.ncpu)
